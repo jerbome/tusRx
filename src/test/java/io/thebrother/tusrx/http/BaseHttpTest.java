b05914864d8f6814f5cad0b4e43d41684984de15
Jerome <jlefrere@salesforce.com>
2017-04-13 13:34:26 +0200

    embedded server from CI

--- src/test/java/io/thebrother/tusrx/http/BaseHttpTest.java
+++ src/test/java/io/thebrother/tusrx/http/BaseHttpTest.java
@@ -7,6 +7,6 @@ import io.thebrother.tusrx.server.TestServerRule;
 public abstract class BaseHttpTest {
 
     @ClassRule
-    public static TestServerRule serverRule = new TestServerRule(false);
+    public static TestServerRule serverRule = new TestServerRule(true);
 
 }
