e7e94640c07a5a13b45ccf9e8da419e3043944b8
Jérôme <jerbome@users.noreply.github.com>
2017-04-19 17:47:00 +0200

    NotImplementedHandlerTest

---
+++ src/test/java/io/thebrother/tusrx/handler/NotImplementedHandlerTest.java
@@ -0,0 +1,30 @@
+package io.thebrother.tusrx.handler;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import io.netty.handler.codec.http.HttpResponseStatus;
+
+import io.thebrother.tusrx.response.TusResponse;
+
+import rx.Observable;
+
+public class NotImplementedHandlerTest extends RequestHandlerTest {
+    @Before
+    public void init() {
+        this.handler = new NotImplementedHandler();
+    }
+
+    @Test
+    public void testResponds501() {
+        // arrange
+
+        // act
+        Observable<TusResponse> response = handler.handle();
+
+        // assert
+        assertThat(response.toBlocking().single().getStatus()).isEqualTo(HttpResponseStatus.NOT_IMPLEMENTED);
+    }
+}
